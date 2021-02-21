e99857ae517df75feef39fc8d47ca05720a2fd76
LiRui <18335929360@163.com>
2021-02-21 20:32:08 +0800

    create MainPlayerAnimInstance class based on AnimInstance

---
+++ Source/GetStarted/Public/Characters/Player/MainPlayerAnimInstance.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MainPlayerAnimInstance.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class GETSTARTED_API UMainPlayerAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(VisibleAnyWhere, BlueprintReadOnly, Category = "Animation Properties")
+		float Speed;
+
+	UPROPERTY(VisibleAnyWhere, BlueprintReadOnly, Category = "Animation Properties")
+		bool bIsInAir;
+
+	UPROPERTY(VisibleAnyWhere, BlueprintReadOnly, Category = "Animation Properties")
+		class AMainPlayer* PlayerRef;
+
+public:
+	virtual void NativeInitializeAnimation() override;
+
+	UFUNCTION(BlueprintCallable, Category = "Animation Properties")
+		void UpdateAnimationProperties();
+};
