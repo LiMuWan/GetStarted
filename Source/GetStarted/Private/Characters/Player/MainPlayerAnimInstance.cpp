8e609ac279873464ae117d30edaa2a1edef944d3
LiRui <18335929360@163.com>
2021-02-21 21:16:00 +0800

    In ABM_MainPlayer reparent blueprint

--- Source/GetStarted/Private/Characters/Player/MainPlayerAnimInstance.cpp
+++ Source/GetStarted/Private/Characters/Player/MainPlayerAnimInstance.cpp
@@ -2,11 +2,27 @@
 
 
 #include "Characters/Player/MainPlayerAnimInstance.h"
+#include "Characters/Player/MainPlayer.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 void UMainPlayerAnimInstance::NativeInitializeAnimation()
 {
+	PlayerRef = Cast<AMainPlayer>(TryGetPawnOwner());
 }
 
 void UMainPlayerAnimInstance::UpdateAnimationProperties()
 {
+	if (PlayerRef == nullptr)
+	{
+		PlayerRef = Cast<AMainPlayer>(TryGetPawnOwner());
+	}
+
+	if (PlayerRef != nullptr)
+	{
+		const FVector SpeedVector = PlayerRef->GetVelocity();
+		const FVector PlanarSpeed = FVector(SpeedVector.X, SpeedVector.Y, 0.0f);
+		Speed = PlanarSpeed.Size();
+
+		bIsInAir = PlayerRef->GetMovementComponent()->IsFalling();
+	}
 }
