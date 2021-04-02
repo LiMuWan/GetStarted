03cc589e479855ad9bfc7a0d7a2d3baae6e4c78f
LiRui <18335929360@163.com>
2021-04-02 22:36:09 +0800

    make  weapon of three color

--- Source/GetStarted/Private/GamePlay/WeaponItem.cpp
+++ Source/GetStarted/Private/GamePlay/WeaponItem.cpp
@@ -3,9 +3,11 @@
 
 #include "GamePlay/WeaponItem.h"
 #include "Components/SkeletalMeshComponent.h"
+#include "Components/SphereComponent.h"
 
 AWeaponItem::AWeaponItem()
 {
+	TriggerVolume->SetSphereRadius(64.0f);
 	DisplayMesh->DestroyComponent();
 	DisplayMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("DisplaySkeletalMesh"));
 	DisplayMesh->SetupAttachment(GetRootComponent());
